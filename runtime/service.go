07eeb83aaeae57f05ee8e902b010f665d4494c83
Maksym Pavlenko <makpav@amazon.com>
2018-12-13 14:24:40 -0800

    Add context to findNextAvailableVsockCID
    Signed-off-by: Maksym Pavlenko <makpav@amazon.com>

--- runtime/service.go
+++ runtime/service.go
@@ -542,11 +542,13 @@ func dialVsock(ctx context.Context, contextID uint32, port uint32) (net.Conn, er
 // It uses VHOST_VSOCK_SET_GUEST_CID ioctl which allows some CID ranges to be statically reserved in advance.
 // The ioctl fails with EADDRINUSE if cid is already taken and with EINVAL if the CID is invalid.
 // Taken from https://bugzilla.redhat.com/show_bug.cgi?id=1291851
-func findNextAvailableVsockCID() (uint32, error) {
+func findNextAvailableVsockCID(ctx context.Context) (uint32, error) {
 	const (
-		ioctlVsockSetGuestCID = uintptr(0x4008AF60) // Corresponds to VHOST_VSOCK_SET_GUEST_CID in vhost.h
-		startCID              = uint32(3)           // 0, 1 and 2 are reserved CIDs, see http://man7.org/linux/man-pages/man7/vsock.7.html
-		maxCID                = math.MaxUint32
+		// Corresponds to VHOST_VSOCK_SET_GUEST_CID in vhost.h
+		ioctlVsockSetGuestCID = uintptr(0x4008AF60)
+		// 0, 1 and 2 are reserved CIDs, see http://man7.org/linux/man-pages/man7/vsock.7.html
+		startCID = 3
+		maxCID   = math.MaxUint32
 	)
 
 	file, err := os.OpenFile("/dev/vhost-vsock", syscall.O_RDWR, 0666)
@@ -557,22 +559,27 @@ func findNextAvailableVsockCID() (uint32, error) {
 	defer file.Close()
 
 	for contextID := startCID; contextID < maxCID; contextID++ {
-		cid := contextID
-		_, _, err = unix.Syscall(
-			unix.SYS_IOCTL,
-			file.Fd(),
-			ioctlVsockSetGuestCID,
-			uintptr(unsafe.Pointer(&cid)))
-
-		switch err {
-		case unix.Errno(0):
-			return contextID, nil
-		case unix.EADDRINUSE:
-			// ID is already taken, try next one
-			continue
+		select {
+		case <-ctx.Done():
+			return 0, ctx.Err()
 		default:
-			// Fail if we get an error we don't expect
-			return 0, err
+			cid := contextID
+			_, _, err = unix.Syscall(
+				unix.SYS_IOCTL,
+				file.Fd(),
+				ioctlVsockSetGuestCID,
+				uintptr(unsafe.Pointer(&cid)))
+
+			switch err {
+			case unix.Errno(0):
+				return uint32(contextID), nil
+			case unix.EADDRINUSE:
+				// ID is already taken, try next one
+				continue
+			default:
+				// Fail if we get an error we don't expect
+				return 0, err
+			}
 		}
 	}
 
@@ -596,7 +603,7 @@ func (s *service) startVM(ctx context.Context, request *taskAPI.CreateTaskReques
 
 	log.G(ctx).Info("starting VM")
 
-	cid, err := findNextAvailableVsockCID()
+	cid, err := findNextAvailableVsockCID(ctx)
 	if err != nil {
 		return nil, err
 	}

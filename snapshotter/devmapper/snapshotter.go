f681bd44c8ddf8c089af1a0841e3a83b256cf3f2
Maksym Pavlenko <makpav@amazon.com>
2019-01-09 16:18:58 -0800

    Rename devmapper snapshotter receiver
    Signed-off-by: Maksym Pavlenko <makpav@amazon.com>

--- snapshotter/devmapper/snapshotter.go
+++ snapshotter/devmapper/snapshotter.go
@@ -86,7 +86,7 @@ func NewSnapshotter(ctx context.Context, configPath string) (*Snapshotter, error
 	}, nil
 }
 
-func (dm *Snapshotter) Stat(ctx context.Context, key string) (snapshots.Info, error) {
+func (s *Snapshotter) Stat(ctx context.Context, key string) (snapshots.Info, error) {
 	log.G(ctx).WithField("key", key).Debug("stat")
 
 	var (
@@ -94,7 +94,7 @@ func (dm *Snapshotter) Stat(ctx context.Context, key string) (snapshots.Info, er
 		err  error
 	)
 
-	err = dm.withTransaction(ctx, false, func(ctx context.Context) error {
+	err = s.withTransaction(ctx, false, func(ctx context.Context) error {
 		_, info, _, err = storage.GetInfo(ctx, key)
 		return err
 	})
@@ -102,11 +102,11 @@ func (dm *Snapshotter) Stat(ctx context.Context, key string) (snapshots.Info, er
 	return info, err
 }
 
-func (dm *Snapshotter) Update(ctx context.Context, info snapshots.Info, fieldpaths ...string) (snapshots.Info, error) {
+func (s *Snapshotter) Update(ctx context.Context, info snapshots.Info, fieldpaths ...string) (snapshots.Info, error) {
 	log.G(ctx).Debugf("update: %s", strings.Join(fieldpaths, ", "))
 
 	var err error
-	err = dm.withTransaction(ctx, true, func(ctx context.Context) error {
+	err = s.withTransaction(ctx, true, func(ctx context.Context) error {
 		info, err = storage.UpdateInfo(ctx, info, fieldpaths...)
 		return err
 	})
@@ -114,13 +114,13 @@ func (dm *Snapshotter) Update(ctx context.Context, info snapshots.Info, fieldpat
 	return info, err
 }
 
-func (dm *Snapshotter) Usage(ctx context.Context, key string) (snapshots.Usage, error) {
+func (s *Snapshotter) Usage(ctx context.Context, key string) (snapshots.Usage, error) {
 	log.G(ctx).WithField("key", key).Debug("usage")
 
 	return snapshots.Usage{}, errors.New("usage not implemented")
 }
 
-func (dm *Snapshotter) Mounts(ctx context.Context, key string) ([]mount.Mount, error) {
+func (s *Snapshotter) Mounts(ctx context.Context, key string) ([]mount.Mount, error) {
 	log.G(ctx).WithField("key", key).Debug("mounts")
 
 	var (
@@ -128,15 +128,15 @@ func (dm *Snapshotter) Mounts(ctx context.Context, key string) ([]mount.Mount, e
 		err  error
 	)
 
-	err = dm.withTransaction(ctx, false, func(ctx context.Context) error {
+	err = s.withTransaction(ctx, false, func(ctx context.Context) error {
 		snap, err = storage.GetSnapshot(ctx, key)
 		return err
 	})
 
-	return dm.buildMounts(snap), nil
+	return s.buildMounts(snap), nil
 }
 
-func (dm *Snapshotter) Prepare(ctx context.Context, key, parent string, opts ...snapshots.Opt) ([]mount.Mount, error) {
+func (s *Snapshotter) Prepare(ctx context.Context, key, parent string, opts ...snapshots.Opt) ([]mount.Mount, error) {
 	log.G(ctx).WithFields(logrus.Fields{"key": key, "parent": parent}).Debug("prepare")
 
 	var (
@@ -144,15 +144,15 @@ func (dm *Snapshotter) Prepare(ctx context.Context, key, parent string, opts ...
 		err    error
 	)
 
-	err = dm.withTransaction(ctx, true, func(ctx context.Context) error {
-		mounts, err = dm.createSnapshot(ctx, snapshots.KindActive, key, parent, opts...)
+	err = s.withTransaction(ctx, true, func(ctx context.Context) error {
+		mounts, err = s.createSnapshot(ctx, snapshots.KindActive, key, parent, opts...)
 		return err
 	})
 
 	return mounts, err
 }
 
-func (dm *Snapshotter) View(ctx context.Context, key, parent string, opts ...snapshots.Opt) ([]mount.Mount, error) {
+func (s *Snapshotter) View(ctx context.Context, key, parent string, opts ...snapshots.Opt) ([]mount.Mount, error) {
 	log.G(ctx).WithFields(logrus.Fields{"key": key, "parent": parent}).Debug("prepare")
 
 	var (
@@ -160,39 +160,39 @@ func (dm *Snapshotter) View(ctx context.Context, key, parent string, opts ...sna
 		err    error
 	)
 
-	err = dm.withTransaction(ctx, true, func(ctx context.Context) error {
-		mounts, err = dm.createSnapshot(ctx, snapshots.KindView, key, parent, opts...)
+	err = s.withTransaction(ctx, true, func(ctx context.Context) error {
+		mounts, err = s.createSnapshot(ctx, snapshots.KindView, key, parent, opts...)
 		return err
 	})
 
 	return mounts, err
 }
 
-func (dm *Snapshotter) Commit(ctx context.Context, name, key string, opts ...snapshots.Opt) error {
+func (s *Snapshotter) Commit(ctx context.Context, name, key string, opts ...snapshots.Opt) error {
 	log.G(ctx).WithFields(logrus.Fields{"name": name, "key": key}).Debug("commit")
 
-	return dm.withTransaction(ctx, true, func(ctx context.Context) error {
+	return s.withTransaction(ctx, true, func(ctx context.Context) error {
 		_, err := storage.CommitActive(ctx, key, name, snapshots.Usage{}, opts...)
 		return err
 	})
 }
 
-func (dm *Snapshotter) Remove(ctx context.Context, key string) error {
+func (s *Snapshotter) Remove(ctx context.Context, key string) error {
 	log.G(ctx).WithField("key", key).Debug("remove")
 
-	return dm.withTransaction(ctx, true, func(ctx context.Context) error {
-		return dm.removeDevice(ctx, key)
+	return s.withTransaction(ctx, true, func(ctx context.Context) error {
+		return s.removeDevice(ctx, key)
 	})
 }
 
-func (dm *Snapshotter) removeDevice(ctx context.Context, key string) error {
+func (s *Snapshotter) removeDevice(ctx context.Context, key string) error {
 	snapID, _, err := storage.Remove(ctx, key)
 	if err != nil {
 		return err
 	}
 
-	deviceName := dm.getDeviceName(snapID)
-	if err := dm.pool.RemoveDevice(ctx, deviceName, true); err != nil {
+	deviceName := s.getDeviceName(snapID)
+	if err := s.pool.RemoveDevice(ctx, deviceName, true); err != nil {
 		log.G(ctx).WithError(err).Errorf("failed to remove device")
 		return err
 	}
@@ -200,19 +200,19 @@ func (dm *Snapshotter) removeDevice(ctx context.Context, key string) error {
 	return nil
 }
 
-func (dm *Snapshotter) Walk(ctx context.Context, fn func(context.Context, snapshots.Info) error) error {
+func (s *Snapshotter) Walk(ctx context.Context, fn func(context.Context, snapshots.Info) error) error {
 	log.G(ctx).Debug("walk")
-	return dm.withTransaction(ctx, false, func(ctx context.Context) error {
+	return s.withTransaction(ctx, false, func(ctx context.Context) error {
 		return storage.WalkInfo(ctx, fn)
 	})
 }
 
-func (dm *Snapshotter) Close() error {
+func (s *Snapshotter) Close() error {
 	log.L.Debug("close")
 
 	var result *multierror.Error
-	dm.closeOnce.Do(func() {
-		for _, fn := range dm.cleanupFn {
+	s.closeOnce.Do(func() {
+		for _, fn := range s.cleanupFn {
 			if err := fn(); err != nil {
 				result = multierror.Append(result, err)
 			}
@@ -222,38 +222,38 @@ func (dm *Snapshotter) Close() error {
 	return result.ErrorOrNil()
 }
 
-func (dm *Snapshotter) createSnapshot(ctx context.Context, kind snapshots.Kind, key, parent string, opts ...snapshots.Opt) ([]mount.Mount, error) {
+func (s *Snapshotter) createSnapshot(ctx context.Context, kind snapshots.Kind, key, parent string, opts ...snapshots.Opt) ([]mount.Mount, error) {
 	snap, err := storage.CreateSnapshot(ctx, kind, key, parent, opts...)
 	if err != nil {
 		return nil, err
 	}
 
 	if len(snap.ParentIDs) == 0 {
-		deviceName := dm.getDeviceName(snap.ID)
+		deviceName := s.getDeviceName(snap.ID)
 		log.G(ctx).Debugf("creating new thin device '%s'", deviceName)
 
-		err := dm.pool.CreateThinDevice(ctx, deviceName, dm.config.BaseImageSizeBytes)
+		err := s.pool.CreateThinDevice(ctx, deviceName, s.config.BaseImageSizeBytes)
 		if err != nil {
 			log.G(ctx).WithError(err).Errorf("failed to create thin device for snapshot %s", snap.ID)
 			return nil, err
 		}
 
-		if err := dm.mkfs(ctx, deviceName); err != nil {
+		if err := s.mkfs(ctx, deviceName); err != nil {
 			return nil, err
 		}
 	} else {
-		parentDeviceName := dm.getDeviceName(snap.ParentIDs[0])
-		snapDeviceName := dm.getDeviceName(snap.ID)
+		parentDeviceName := s.getDeviceName(snap.ParentIDs[0])
+		snapDeviceName := s.getDeviceName(snap.ID)
 		log.G(ctx).Debugf("creating snapshot device '%s' from '%s'", snapDeviceName, parentDeviceName)
 
-		err := dm.pool.CreateSnapshotDevice(ctx, parentDeviceName, snapDeviceName, dm.config.BaseImageSizeBytes)
+		err := s.pool.CreateSnapshotDevice(ctx, parentDeviceName, snapDeviceName, s.config.BaseImageSizeBytes)
 		if err != nil {
 			log.G(ctx).WithError(err).Errorf("failed to create snapshot device from parent %s", parentDeviceName)
 			return nil, err
 		}
 	}
 
-	mounts := dm.buildMounts(snap)
+	mounts := s.buildMounts(snap)
 
 	// Remove default directories not expected by the container image
 	_ = mount.WithTempMount(ctx, mounts, func(root string) error {
@@ -263,7 +263,7 @@ func (dm *Snapshotter) createSnapshot(ctx context.Context, kind snapshots.Kind, 
 	return mounts, nil
 }
 
-func (dm *Snapshotter) mkfs(ctx context.Context, deviceName string) error {
+func (s *Snapshotter) mkfs(ctx context.Context, deviceName string) error {
 	args := []string{
 		"-E",
 		// We don't want any zeroing in advance when running mkfs on thin devices (see "man mkfs.ext4")
@@ -282,17 +282,17 @@ func (dm *Snapshotter) mkfs(ctx context.Context, deviceName string) error {
 	return nil
 }
 
-func (dm *Snapshotter) getDeviceName(snapID string) string {
+func (s *Snapshotter) getDeviceName(snapID string) string {
 	// Add pool name as prefix to avoid collisions with devices from other pools
-	return fmt.Sprintf("%s-snap-%s", dm.config.PoolName, snapID)
+	return fmt.Sprintf("%s-snap-%s", s.config.PoolName, snapID)
 }
 
-func (dm *Snapshotter) getDevicePath(snap storage.Snapshot) string {
-	name := dm.getDeviceName(snap.ID)
+func (s *Snapshotter) getDevicePath(snap storage.Snapshot) string {
+	name := s.getDeviceName(snap.ID)
 	return dmsetup.GetFullDevicePath(name)
 }
 
-func (dm *Snapshotter) buildMounts(snap storage.Snapshot) []mount.Mount {
+func (s *Snapshotter) buildMounts(snap storage.Snapshot) []mount.Mount {
 	var options []string
 
 	if snap.Kind != snapshots.KindActive {
@@ -301,7 +301,7 @@ func (dm *Snapshotter) buildMounts(snap storage.Snapshot) []mount.Mount {
 
 	mounts := []mount.Mount{
 		{
-			Source:  dm.getDevicePath(snap),
+			Source:  s.getDevicePath(snap),
 			Type:    fsTypeExt4,
 			Options: options,
 		},
@@ -312,8 +312,8 @@ func (dm *Snapshotter) buildMounts(snap storage.Snapshot) []mount.Mount {
 
 // withTransaction wraps fn callback with containerd's meta store transaction.
 // If callback returns an error or transaction is not writable, database transaction will be discarded.
-func (dm *Snapshotter) withTransaction(ctx context.Context, writable bool, fn func(ctx context.Context) error) error {
-	ctx, trans, err := dm.store.TransactionContext(ctx, writable)
+func (s *Snapshotter) withTransaction(ctx context.Context, writable bool, fn func(ctx context.Context) error) error {
+	ctx, trans, err := s.store.TransactionContext(ctx, writable)
 	if err != nil {
 		return err
 	}

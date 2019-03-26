a6394b00fcbbd81a5ecc634974557f4b1cc76a92
Maksym Pavlenko <makpav@amazon.com>
2019-03-25 18:17:20 -0700

    Add Usage test
    Signed-off-by: Maksym Pavlenko <makpav@amazon.com>

--- snapshotter/devmapper/snapshotter_test.go
+++ snapshotter/devmapper/snapshotter_test.go
@@ -16,13 +16,19 @@ package devmapper
 import (
 	"context"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"testing"
 	"time"
 
+	"github.com/containerd/containerd/mount"
+	"github.com/containerd/containerd/namespaces"
 	"github.com/containerd/containerd/snapshots"
 	"github.com/containerd/containerd/snapshots/testsuite"
+	"github.com/containerd/continuity/fs/fstest"
 	"github.com/hashicorp/go-multierror"
 	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
 	"github.com/firecracker-microvm/firecracker-containerd/internal"
@@ -34,7 +40,7 @@ func TestSnapshotterSuite(t *testing.T) {
 	internal.RequiresRoot(t)
 	logrus.SetLevel(logrus.DebugLevel)
 
-	testsuite.SnapshotterSuite(t, "devmapper", func(ctx context.Context, root string) (snapshots.Snapshotter, func() error, error) {
+	snapshotterFn := func(ctx context.Context, root string) (snapshots.Snapshotter, func() error, error) {
 		// Create loopback devices for each test case
 		_, loopDataDevice := createLoopbackDevice(t, root)
 		_, loopMetaDevice := createLoopbackDevice(t, root)
@@ -67,5 +73,63 @@ func TestSnapshotterSuite(t *testing.T) {
 		snap.cleanupFn = append([]closeFunc{removePool}, snap.cleanupFn...)
 
 		return snap, snap.Close, nil
+	}
+
+	testsuite.SnapshotterSuite(t, "devmapper", snapshotterFn)
+
+	ctx := context.Background()
+	ctx = namespaces.WithNamespace(ctx, "testsuite")
+
+	t.Run("DevMapperUsage", func(t *testing.T) {
+		tempDir, err := ioutil.TempDir("", "snapshot-suite-usage")
+		require.NoError(t, err)
+		defer os.RemoveAll(tempDir)
+
+		snapshotter, closer, err := snapshotterFn(ctx, tempDir)
+		require.NoError(t, err)
+		defer closer()
+
+		testUsage(t, snapshotter)
 	})
+}
+
+// testUsage tests devmapper's Usage implementation. This is an approximate test as it's hard to
+// predict how many blocks will be consumed under different conditions and parameters.
+func testUsage(t *testing.T, snapshotter snapshots.Snapshotter) {
+	ctx := context.Background()
+
+	// Create empty base layer
+	_, err := snapshotter.Prepare(ctx, "prepare-1", "")
+	require.NoError(t, err)
+
+	emptyLayerUsage, err := snapshotter.Usage(ctx, "prepare-1")
+	assert.NoError(t, err)
+
+	// Should be > 0 as just written file system also consumes blocks
+	assert.NotZero(t, emptyLayerUsage.Size)
+
+	err = snapshotter.Commit(ctx, "layer-1", "prepare-1")
+	require.NoError(t, err)
+
+	// Create child layer with 1MB file
+
+	var (
+		sizeBytes   int64 = 1048576 // 1MB
+		baseApplier       = fstest.Apply(fstest.CreateRandomFile("/a", 12345679, sizeBytes, 0777))
+	)
+
+	mounts, err := snapshotter.Prepare(ctx, "prepare-2", "layer-1")
+	require.NoError(t, err)
+
+	err = mount.WithTempMount(ctx, mounts, baseApplier.Apply)
+	require.NoError(t, err)
+
+	err = snapshotter.Commit(ctx, "layer-2", "prepare-2")
+	require.NoError(t, err)
+
+	layer2Usage, err := snapshotter.Usage(ctx, "layer-2")
+	require.NoError(t, err)
+
+	// Should be at least 1 MB + fs metadata
+	assert.InDelta(t, sizeBytes, layer2Usage.Size, 256*dmsetup.SectorSize)
 }

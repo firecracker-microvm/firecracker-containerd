b2c8ff8e30c0fe7673f48a1d8af60bfd0352fbfe
Erik Sipsma <sipsma@amazon.com>
2019-04-09 08:01:56 +0000

    Support specifying VMID when creating containers.
    This is the first step for updating shims to support multiple containers
    per-VM. It includes support for clients providing VMID and for the host
    shim to parse that VMID, using it to decide whether a new shim needs to
    spun up for the VM or if an existing one can be re-used.
    
    Full support for multiple containers in a single VM requires more
    changes in Agent; this change in particular just gives full support for
    assigning single individual containers to different VMs.
    
    Signed-off-by: Erik Sipsma <sipsma@amazon.com>

--- go.mod
+++ go.mod
@@ -2,17 +2,16 @@ module github.com/firecracker-microvm/firecracker-containerd
 
 require (
 	github.com/BurntSushi/toml v0.3.1 // indirect
-	github.com/Microsoft/go-winio v0.4.11 // indirect
+	github.com/Microsoft/go-winio v0.4.12 // indirect
 	github.com/Microsoft/hcsshim v0.8.1 // indirect
 	github.com/StackExchange/wmi v0.0.0-20181212234831-e0a55b97c705 // indirect
-	github.com/boltdb/bolt v1.3.1 // indirect
 	github.com/containerd/cgroups v0.0.0-20181105182409-82cb49fc1779 // indirect
 	github.com/containerd/console v0.0.0-20181022165439-0650fd9eeb50 // indirect
 	github.com/containerd/containerd v1.2.0
 	github.com/containerd/continuity v0.0.0-20181027224239-bea7585dbfac
 	github.com/containerd/cri v1.11.1 // indirect
 	github.com/containerd/fifo v0.0.0-20180307165137-3d5202aec260
-	github.com/containerd/go-runc v0.0.0-20180907222934-5a6d9f37cfa3 // indirect
+	github.com/containerd/go-runc v0.0.0-20190226155025-7d11b49dc076 // indirect
 	github.com/containerd/ttrpc v0.0.0-20181001154009-f51df4475b76
 	github.com/containerd/typeurl v0.0.0-20181015155603-461401dc8f19
 	github.com/coreos/go-systemd v0.0.0-20181031085051-9002847aa142 // indirect
@@ -24,6 +23,7 @@ require (
 	github.com/firecracker-microvm/firecracker-go-sdk v0.15.2-0.20190321195537-b704cc3feb1e
 	github.com/go-ole/go-ole v1.2.4 // indirect
 	github.com/godbus/dbus v0.0.0-20181025153459-66d97aec3384 // indirect
+	github.com/gofrs/uuid v3.2.0+incompatible
 	github.com/gogo/googleapis v1.1.0 // indirect
 	github.com/gogo/protobuf v1.2.1
 	github.com/golang/mock v1.1.1
@@ -44,7 +44,7 @@ require (
 	github.com/stretchr/testify v1.2.2
 	github.com/syndtr/gocapability v0.0.0-20180916011248-d98352740cb2 // indirect
 	github.com/urfave/cli v1.20.0 // indirect
-	go.etcd.io/bbolt v1.3.0
+	go.etcd.io/bbolt v1.3.1-etcd.8
 	golang.org/x/crypto v0.0.0-20190325154230-a5d413f7728c // indirect
 	golang.org/x/net v0.0.0-20190327214358-63eda1eb0650
 	golang.org/x/sync v0.0.0-20181108010431-42b317875d0f
--- runtime/service_integ_test.go
+++ runtime/service_integ_test.go
@@ -14,9 +14,16 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"context"
 	"fmt"
+	"io/ioutil"
+	"os/exec"
 	"path/filepath"
+	"strconv"
+	"strings"
+	"sync"
 	"syscall"
 	"testing"
 	"time"
@@ -28,10 +35,14 @@ import (
 	"github.com/containerd/containerd/oci"
 	"github.com/containerd/containerd/runtime"
 	"github.com/containerd/typeurl"
+	"github.com/opencontainers/runtime-spec/specs-go"
+	"github.com/pkg/errors"
 	"github.com/shirou/gopsutil/process"
 	"github.com/stretchr/testify/require"
 
 	"github.com/firecracker-microvm/firecracker-containerd/internal"
+	"github.com/firecracker-microvm/firecracker-containerd/proto"
+	"github.com/firecracker-microvm/firecracker-containerd/runtime/firecrackeroci"
 )
 
 const (
@@ -41,6 +52,9 @@ const (
 	firecrackerRuntime   = "aws.firecracker"
 	naiveSnapshotterName = "firecracker-naive"
 	shimProcessName      = "containerd-shim-aws-firecracker"
+
+	defaultVMRootfsPath = "/var/lib/firecracker-containerd/runtime/hello-rootfs.ext4"
+	defaultVMNetDevName = "eth0"
 )
 
 func TestShimExitsUponContainerKill_Isolated(t *testing.T) {
@@ -115,9 +129,162 @@ func TestShimExitsUponContainerKill_Isolated(t *testing.T) {
 
 		err = internal.WaitForPidToExit(testCtx, time.Second, shimProcess.Pid)
 		require.NoError(t, err, "failed waiting for shim process \"%s\" to exit", shimProcessName)
+
+		varRunFCContents, err := ioutil.ReadDir(varRunDir)
+		require.NoError(t, err, `failed to list directory "%s"`, varRunDir)
+		// there should just be one file left in the directory for the naive snapshotter socket
+		require.Len(t, varRunFCContents, 1, "expect %s to be cleared after shims shutdown", varRunDir)
 	case err = <-exitEventErrCh:
 		require.Fail(t, "unexpected error", "unexpectedly received on task exit error channel: %s", err.Error())
 	case <-testCtx.Done():
 		require.Fail(t, "context canceled", "context canceled while waiting for container \"%s\" exit: %s", containerName, testCtx.Err())
 	}
+}
+
+// vmIDtoMacAddr converts a provided VMID to a unique Mac Address. This is a convenient way of providing the VMID to clients within
+// the VM without the extra complication of alternative approaches like MMDS.
+func vmIDtoMacAddr(vmID uint) string {
+	var addrParts []string
+
+	// mac addresses have 6 hex components separate by ":", i.e. "11:22:33:44:55:66"
+	numMacAddrComponents := uint(6)
+
+	for n := uint(0); n < numMacAddrComponents; n++ {
+		// To isolate the value of the nth component, right bit shift the vmID by 8*n (there are 8 bits per component) and
+		// mask out any upper bits leftover (bitwise AND with 255)
+		addrComponent := (vmID >> (8 * n)) & 255
+
+		// format the component as a two-digit hex string
+		addrParts = append(addrParts, fmt.Sprintf("%02x", addrComponent))
+	}
+
+	return strings.Join(addrParts, ":")
+}
+
+func createTapDevice(ctx context.Context, tapName string) error {
+	err := exec.CommandContext(ctx, "ip", "tuntap", "add", tapName, "mode", "tap").Run()
+	if err != nil {
+		return errors.Wrapf(err, "failed to create tap device %s", tapName)
+	}
+
+	err = exec.CommandContext(ctx, "ip", "link", "set", tapName, "up").Run()
+	if err != nil {
+		return errors.Wrapf(err, "failed to up tap device %s", tapName)
+	}
+
+	return nil
+}
+
+func TestMultipleVMs_Isolated(t *testing.T) {
+	internal.RequiresIsolation(t)
+
+	const (
+		numVMs = 5
+		// TODO update with multiple containers per-VM once that PR is merged
+		containersPerVM = 1
+	)
+
+	ctx := namespaces.WithNamespace(context.Background(), "default")
+
+	client, err := containerd.New(containerdSockPath, containerd.WithDefaultRuntime(firecrackerRuntime))
+	require.NoError(t, err, "unable to create client to containerd service at %s, is containerd running?", containerdSockPath)
+	defer client.Close()
+
+	image, err := client.Pull(ctx, debianDockerImage, containerd.WithPullUnpack, containerd.WithPullSnapshotter(naiveSnapshotterName))
+	require.NoError(t, err, "failed to pull image %s, is the the %s snapshotter running?", debianDockerImage, naiveSnapshotterName)
+
+	rootfsBytes, err := ioutil.ReadFile(defaultVMRootfsPath)
+	require.NoError(t, err, "failed to read rootfs file")
+
+	// This test spawns separate VMs in parallel and ensures containers are spawned within each expected VM. It asserts each
+	// container ends up in the right VM by assigning each VM a network device with a unique mac address and having each container
+	// print the mac address it sees inside its VM.
+	var vmWg sync.WaitGroup
+	for vmID := 0; vmID < numVMs; vmID++ {
+		vmWg.Add(1)
+		go func(vmID int) {
+			defer vmWg.Done()
+
+			tapName := fmt.Sprintf("tap%d", vmID)
+			err = createTapDevice(ctx, tapName)
+			require.NoError(t, err, "failed to create tap device for vm %d", vmID)
+
+			// TODO once Noah's immutable rootfs change is merged, we can use that as our rootfs for all VMs instead of copying
+			// one per-VM
+			rootfsPath := fmt.Sprintf("%s.%d", defaultVMRootfsPath, vmID)
+			err = ioutil.WriteFile(rootfsPath, rootfsBytes, 0600)
+			require.NoError(t, err, "failed to copy vm rootfs to %s", rootfsPath)
+
+			var containerWg sync.WaitGroup
+			for containerID := 0; containerID < containersPerVM; containerID++ {
+				containerWg.Add(1)
+				go func(containerID int) {
+					defer containerWg.Done()
+					containerName := fmt.Sprintf("container-%d-%d", vmID, containerID)
+					snapshotName := fmt.Sprintf("snapshot-%d-%d", vmID, containerID)
+
+					// spawn a container that just prints the VM's eth0 mac address (which we have set uniquely per VM)
+					newContainer, err := client.NewContainer(ctx,
+						containerName,
+						containerd.WithSnapshotter(naiveSnapshotterName),
+						containerd.WithNewSnapshot(snapshotName, image),
+						containerd.WithNewSpec(
+							oci.WithProcessArgs("cat", fmt.Sprintf("/sys/class/net/%s/address", defaultVMNetDevName)),
+							oci.WithHostNamespace(specs.NetworkNamespace),
+							firecrackeroci.WithVMID(strconv.Itoa(vmID)),
+						),
+					)
+					require.NoError(t, err, "failed to create container %s", containerName)
+
+					var stdout bytes.Buffer
+					var stderr bytes.Buffer
+
+					newTask, err := newContainer.NewTask(ctx,
+						cio.NewCreator(cio.WithStreams(nil, bufio.NewWriter(&stdout), bufio.NewWriter(&stderr))),
+						func(ctx context.Context, _ *containerd.Client, ti *containerd.TaskInfo) error {
+							ti.Options = &proto.FirecrackerConfig{
+								RootDrive: &proto.FirecrackerDrive{
+									PathOnHost:   rootfsPath,
+									IsReadOnly:   false,
+									IsRootDevice: true,
+								},
+								NetworkInterfaces: []*proto.FirecrackerNetworkInterface{
+									{
+										HostDevName: tapName,
+										MacAddress:  vmIDtoMacAddr(uint(vmID)),
+									},
+								},
+							}
+							return nil
+						})
+					require.NoError(t, err, "failed to create task for container %s", containerName)
+
+					err = newTask.Start(ctx)
+					require.NoError(t, err, "failed to start task for container %s", containerName)
+
+					exitCh, err := newTask.Wait(ctx)
+					require.NoError(t, err, "failed to wait on task for container %s", containerName)
+
+					select {
+					case exitStatus := <-exitCh:
+						// if there was anything on stderr, print it to assist debugging
+						stderrOutput := stderr.String()
+						if len(stderrOutput) != 0 {
+							fmt.Printf("stderr output from vm %d, container %d: %s", vmID, containerID, stderrOutput)
+						}
+
+						require.Equal(t, uint32(0), exitStatus.ExitCode())
+						require.Equal(t, vmIDtoMacAddr(uint(vmID)), strings.TrimSpace(stdout.String()))
+					case <-ctx.Done():
+						require.Fail(t, "context cancelled",
+							"context cancelled while waiting for container %s to exit, err: %v", containerName, ctx.Err())
+					}
+				}(containerID)
+			}
+
+			containerWg.Wait()
+		}(vmID)
+	}
+
+	vmWg.Wait()
 }

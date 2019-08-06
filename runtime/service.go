0f7744ff7f2ee832ed870ee43735b4a0c67a5066
Kazuyoshi Kato <katokazu@amazon.com>
2019-08-06 13:31:29 -0700

    Ignore `appendAssign: append result not assigned to the same slice`
    Signed-off-by: Kazuyoshi Kato <katokazu@amazon.com>

--- runtime/service.go
+++ runtime/service.go
@@ -665,6 +665,7 @@ func (s *service) buildVMConfiguration(req *proto.CreateVMRequest) (*firecracker
 	}
 
 	// a micro VM must know all drives
+	// nolint: gocritic
 	cfg.Drives = append(stubDrives, driveBuilder.Build()...)
 
 	// Setup network interfaces

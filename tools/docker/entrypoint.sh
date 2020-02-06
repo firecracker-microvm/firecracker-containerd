ce42c4e0375fb5ea5dbdb5c392b3d19aca352392
Kazuyoshi Kato <katokazu@amazon.com>
2020-02-06 13:59:11 -0800

    Remove FICD_SNAPSHOTTER and FICD_SNAPSHOTTER_OUTFILE
    The only snapshotter we can use right now is devmapper.
    
    Signed-off-by: Kazuyoshi Kato <katokazu@amazon.com>

--- tools/docker/entrypoint.sh
+++ tools/docker/entrypoint.sh
@@ -5,20 +5,12 @@ chmod a+rwx ${FICD_LOG_DIR}
 
 mkdir -p /etc/containerd/snapshotter
 
-case "$FICD_SNAPSHOTTER" in
-    devmapper)
-        cat > /etc/containerd/snapshotter/devmapper.toml <<EOF
+cat > /etc/containerd/snapshotter/devmapper.toml <<EOF
 [plugins]
   [plugins.devmapper]
     pool_name = "fcci--vg-${FICD_DM_POOL}"
     base_image_size = "1024MB"
 EOF
-        ;;
-    *)
-        echo "This Docker image doesn't support $FICD_SNAPSHOTTER snapshotter"
-        exit 1
-        ;;
-esac
 
 touch ${FICD_CONTAINERD_OUTFILE}
 chmod a+rw ${FICD_CONTAINERD_OUTFILE}

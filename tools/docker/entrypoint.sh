290eaf2c1fdc89dad5efa02a395e93cbaa4aedde
Kazuyoshi Kato <katokazu@amazon.com>
2020-02-06 17:20:02 -0800

    Don't assume "fcci--vg-" prefix in tools/docker/entrypoint.sh
    Our EC2 instance has this volume group, but other environments would
    not.
    
    Signed-off-by: Kazuyoshi Kato <katokazu@amazon.com>

--- tools/docker/entrypoint.sh
+++ tools/docker/entrypoint.sh
@@ -5,10 +5,16 @@ chmod a+rwx ${FICD_LOG_DIR}
 
 mkdir -p /etc/containerd/snapshotter
 
+if [[ -z "$FICD_DM_VOLUME_GROUP" ]]; then
+   pool_name="${FICD_DM_POOL}"
+else
+   pool_name="$(echo "$FICD_DM_VOLUME_GROUP" | sed s/-/--/g)-${FICD_DM_POOL}"
+fi
+
 cat > /etc/containerd/snapshotter/devmapper.toml <<EOF
 [plugins]
   [plugins.devmapper]
-    pool_name = "fcci--vg-${FICD_DM_POOL}"
+    pool_name = "${pool_name}"
     base_image_size = "1024MB"
 EOF
 

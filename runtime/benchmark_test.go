ace03d7a1e878941ade3f9529356fa3dc7abb08b
Kazuyoshi Kato <katokazu@amazon.com>
2020-03-02 09:24:32 -0800

    Benchmark a paralell VM launch case
    It should be slower, and it is.
    
    Signed-off-by: Kazuyoshi Kato <katokazu@amazon.com>

--- runtime/benchmark_test.go
+++ runtime/benchmark_test.go
@@ -16,6 +16,7 @@ package main
 import (
 	"context"
 	"testing"
+	"time"
 
 	"github.com/containerd/containerd"
 	"github.com/containerd/containerd/namespaces"
@@ -26,7 +27,32 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func benchmarkCreateAndStopVM(b *testing.B, vcpuCount uint32, kernelArgs string) {
+func createAndStopVM(
+	ctx context.Context,
+	fcClient fccontrol.FirecrackerService,
+	request proto.CreateVMRequest,
+) error {
+	uuid, err := uuid.NewV4()
+	if err != nil {
+		return err
+	}
+
+	request.VMID = uuid.String()
+
+	_, err = fcClient.CreateVM(ctx, &request)
+	if err != nil {
+		return err
+	}
+
+	_, err = fcClient.StopVM(ctx, &proto.StopVMRequest{VMID: request.VMID})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func benchmarkCreateAndStopVM(b *testing.B, vcpuCount uint32, kernelArgs string, parallel int) {
 	require := require.New(b)
 
 	client, err := containerd.New(containerdSockPath, containerd.WithDefaultRuntime(firecrackerRuntime))
@@ -39,34 +65,46 @@ func benchmarkCreateAndStopVM(b *testing.B, vcpuCount uint32, kernelArgs string)
 	require.NoError(err, "failed to create ttrpc client")
 
 	fcClient := fccontrol.NewFirecrackerClient(pluginClient.Client())
+	request := proto.CreateVMRequest{
+		KernelArgs: kernelArgs,
+		MachineCfg: &proto.FirecrackerMachineConfiguration{
+			VcpuCount: vcpuCount,
+		},
+	}
 
-	for i := 0; i < b.N; i++ {
-		uuid, err := uuid.NewV4()
-		require.NoError(err)
+	ch := make(chan error, parallel)
+	for i := 0; i < parallel; i++ {
+		go func() {
+			var err error
+			defer func() {
+				ch <- err
+			}()
 
-		vmID := uuid.String()
+			for i := 0; i < b.N; i++ {
+				ctx, cancel := context.WithTimeout(ctx, 5*time.Second)
+				defer cancel()
 
-		_, err = fcClient.CreateVM(ctx, &proto.CreateVMRequest{
-			VMID:       vmID,
-			KernelArgs: kernelArgs,
-			MachineCfg: &proto.FirecrackerMachineConfiguration{
-				VcpuCount: vcpuCount,
-			},
-		})
-		require.NoError(err)
+				err = createAndStopVM(ctx, fcClient, request)
+				if err != nil {
+					return
+				}
+			}
+		}()
+	}
 
-		_, err = fcClient.StopVM(ctx, &proto.StopVMRequest{VMID: vmID})
-		require.NoError(err)
+	// Make sure all goroutines are finished successfully.
+	for i := 0; i < parallel; i++ {
+		require.NoError(<-ch)
 	}
 }
 
 func BenchmarkCreateAndStopVM_Vcpu1_Isolated(b *testing.B) {
 	prepareIntegTest(b)
-	benchmarkCreateAndStopVM(b, 1, defaultRuntimeConfig.KernelArgs)
+	benchmarkCreateAndStopVM(b, 1, defaultRuntimeConfig.KernelArgs, 1)
 }
 func BenchmarkCreateAndStopVM_Vcpu5_Isolated(b *testing.B) {
 	prepareIntegTest(b)
-	benchmarkCreateAndStopVM(b, 5, defaultRuntimeConfig.KernelArgs)
+	benchmarkCreateAndStopVM(b, 5, defaultRuntimeConfig.KernelArgs, 1)
 }
 
 func BenchmarkCreateAndStopVM_Quiet_Isolated(b *testing.B) {
@@ -76,5 +114,10 @@ func BenchmarkCreateAndStopVM_Quiet_Isolated(b *testing.B) {
 		1,
 		// Same as https://github.com/firecracker-microvm/firecracker-demo/blob/c22499567b63b4edd85e19ca9b0e9fa398b3300b/start-firecracker.sh#L9
 		"ro noapic reboot=k panic=1 pci=off nomodules systemd.log_color=false systemd.unit=firecracker.target init=/sbin/overlay-init tsc=reliable quiet 8250.nr_uarts=0 ipv6.disable=1",
+		1,
 	)
+}
+func BenchmarkCreateAndStopVM_Parallel10_Isolated(b *testing.B) {
+	prepareIntegTest(b)
+	benchmarkCreateAndStopVM(b, 1, defaultRuntimeConfig.KernelArgs, 10)
 }

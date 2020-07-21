990b3f52e3322c51a68f6508465946867d582c02
Kazuyoshi Kato <katokazu@amazon.com>
2020-07-21 11:26:25 -0700

    Make FICD_DM_POOL optional
    By default, thinpool.sh creates a loopback-device based thin pool.
    
    Signed-off-by: Kazuyoshi Kato <katokazu@amazon.com>

--- tools/thinpool.sh
+++ tools/thinpool.sh
@@ -15,7 +15,11 @@
 
 FICD_DM_VOLUME_GROUP="$FICD_DM_VOLUME_GROUP"
 
-set -eu
+# The tmp/devmapper/ directory will be created on this project's
+# root directory.
+DIR=$(dirname $BASH_SOURCE)/../tmp/devmapper
+
+set -euo pipefail
 
 subcommand="$1"
 name="$2"
@@ -24,21 +28,55 @@ if [ -z "$name" ]; then
     exit 0
 fi
 
+create_loopback_device() {
+    local path=$1
+    local size=$2
+
+    if [[ ! -f "$path" ]]; then
+        touch "$path"
+        truncate -s "$size" "$path"
+    fi
+
+    local dev=$(sudo losetup --output NAME --noheadings --associated "$path")
+    if [[ -z "$dev" ]]; then
+        dev=$(sudo losetup --find --show $path)
+    fi
+    echo $dev
+}
+
 if [[ -z "$FICD_DM_VOLUME_GROUP" ]]; then
     pool_create() {
-        echo
+        mkdir -p $DIR
+
+        local datadev=$(create_loopback_device "$DIR/data" '10G')
+        local metadev=$(create_loopback_device "$DIR/metadata" '1G')
+
+        local sectorsize=512
+        local datasize="$(sudo blockdev --getsize64 -q ${datadev})"
+        local length_sectors=$(bc <<< "${datasize}/${sectorsize}")
+        local thinp_table="0 ${length_sectors} thin-pool ${metadev} ${datadev} 128 32768 1 skip_block_zeroing"
+        sudo dmsetup create "$name" --table "${thinp_table}"
     }
 
     pool_remove() {
-        echo
-    }
+        for snapshot in $(sudo dmsetup ls | awk "/^$name-snap-/ { print \$1 }"); do
+            sudo dmsetup remove $snapshot
+        done
 
-    pool_reset() {
         local dev_no=1
         while true; do
             sudo dmsetup message "$name" 0 "delete $dev_no" || break
             dev_no=$(($dev_no + 1))
         done
+
+        sudo dmsetup remove "$name"
+    }
+
+    pool_reset() {
+        if sudo dmsetup info "$name"; then
+            pool_remove
+        fi
+        pool_create
     }
 else
     dm_device="/dev/mapper/$(echo ${FICD_DM_VOLUME_GROUP} | sed -e s/-/--/g)-$name"

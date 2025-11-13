fc035cd635fe595d5c1599065e7cf632920bcc94
Justin Alvarez <alvajus@amazon.com>
2025-11-13 19:10:33 +0000

    explicitly set path when executing containerd service
    Signed-off-by: Justin Alvarez <alvajus@amazon.com>

--- tools/docker/entrypoint.sh
+++ tools/docker/entrypoint.sh
@@ -66,7 +66,7 @@ EOF
 
 touch ${FICD_CONTAINERD_OUTFILE}
 chmod a+rw ${FICD_CONTAINERD_OUTFILE}
-/usr/local/bin/containerd --log-level debug &>> ${FICD_CONTAINERD_OUTFILE} &
+PATH="/bin:/usr/bin:/usr/local/bin:/sbin:/usr/sbin:/usr/local/sbin:/usr/local/go/bin" /usr/local/bin/containerd --log-level debug &>> ${FICD_CONTAINERD_OUTFILE} &
 
 /usr/local/bin/http-address-resolver &>> ${FICD_LOG_DIR}/http-address-resolver.out &
 /usr/local/bin/demux-snapshotter &>> ${FICD_LOG_DIR}/demux-snapshotter.out &

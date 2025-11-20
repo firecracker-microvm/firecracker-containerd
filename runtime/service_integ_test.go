2bbb56806af680a35d5fd449141baf16b2411f62
Justin Alvarez <alvajus@amazon.com>
2025-11-20 17:54:38 +0000

    Make memory overallocation less extreme
    Signed-off-by: Justin Alvarez <alvajus@amazon.com>

--- runtime/service_integ_test.go
+++ runtime/service_integ_test.go
@@ -2169,10 +2169,10 @@ func TestOOM_Isolated(t *testing.T) {
 		containerd.WithSnapshotter(defaultSnapshotterName),
 		containerd.WithNewSnapshot("snapshot-"+vmID, image),
 		containerd.WithNewSpec(
-			// The container is having 3MB of memory.
+			// The container is having 4MB of memory.
 			oci.WithMemoryLimit(4*1024*1024),
 			// But the dd command allocates 10MB of data on memory, which will be OOM killed.
-			oci.WithProcessArgs("/bin/dd", "if=/dev/zero", "ibs=1000M", "of=/dev/null"),
+			oci.WithProcessArgs("/bin/dd", "if=/dev/zero", "ibs=10M", "of=/dev/null"),
 			firecrackeroci.WithVMID(vmID),
 		),
 	)
